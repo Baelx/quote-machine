b9394f0064723effa1a6ce8974cf2087798a45dd
Alex <awintschel@gmail.com>
2018-02-18 12:23:35 -0500

    Finished about button scripts

--- assets/main.js
+++ assets/main.js
@@ -5,6 +5,30 @@ var aboutButton = document.getElementById("about-button");
 var aboutArea = document.getElementById("about-area");
 var body = document.getElementById("body");
 var wrapper = document.getElementById("wrapper");
+var disclaimer = document.getElementById("disclaimer");
+var credits = document.getElementById("credits");
+var resources = document.getElementById("resources");
+
+var quoteRequest = new XMLHttpRequest();
+quoteRequest.open("GET", "https://api.whatdoestrumpthink.com/api/v1/quotes/random", true);
+quoteRequest.onload = function() {
+  if (quoteRequest.status == 200) {
+    var data = JSON.parse(quoteRequest.responseText);
+    quote.innerHTML = data.message;
+  }
+};
+quoteRequest.send();
+
+var imgRequest = new XMLHttpRequest();
+imgRequest.open("GET", "https://api.unsplash.com/photos/random/?client_id=a476048633d1fb233b59080593173ffa6de15ab3670e2b9ddbe593d3137d2e31", true);
+imgRequest.onload = function() {
+  if (imgRequest.status == 200) {
+    var data = JSON.parse(imgRequest.responseText);
+    var newBg = "url(" + data.urls.regular + ")";
+    quoteArea.style.background = newBg;
+  }
+};
+imgRequest.send();
 
 
 aboutButton.addEventListener("click", function(event){
@@ -12,21 +36,28 @@ aboutButton.addEventListener("click", function(event){
       quoteArea.classList.contains("blur-bg-img") &&
       aboutArea.classList.contains("show-about-area") &&
       wrapper.classList.contains("dark-bg") &&
-      body.classList.contains("dark-bg")
+      body.classList.contains("dark-bg") &&
+      !disclaimer.classList.contains("hide-elem") &&
+      !credits.classList.contains("hide-elem") &&
+      !resources.classList.contains("hide-elem")
       ){
       quote.className = "";
       quoteArea.className = "";
       aboutArea.className = "";
       wrapper.className = "";
       body.className = "";
-      console.log("added classes");
+      disclaimer.className = " hide-elem ";
+      credits.className = " hide-elem ";
+      resources.className = " hide-elem ";
   } else {
       quote.className += " hide-elem ";
       quoteArea.className += " blur-bg-img ";
       aboutArea.className = " show-about-area ";
       wrapper.className = " dark-bg ";
       body.className = " dark-bg ";
-      console.log("removed classes");
+      disclaimer.className = "";
+      credits.className = "";
+      resources.className = "";
   }
 })
 
