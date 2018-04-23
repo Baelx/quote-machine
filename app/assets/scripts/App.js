011358b53f06820007519f7ce79287cd70c8de72
Alex <awintschel@gmail.com>
2018-04-23 15:01:54 -0400

    base styles complete

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,11 @@
+import NewQuote from './modules/NewQuote';
+import RevealAbout from './modules/RevealAbout';
+import $ from 'jquery';
+
+// const newQuote = new NewQuote();
+const revealAbout = new RevealAbout();
+
+
+// new RevealOnScroll($(".feature-item"), "85%");
+// new RevealOnScroll($(".testimonial"), "60%");
+// const modal = new Modal();
