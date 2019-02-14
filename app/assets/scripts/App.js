05fe06fd759112be60823e5c7f2776afba61c4db
Alex <awintschel@gmail.com>
2019-02-13 21:36:58 -0800

    refactored js for smoother resource loading

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,21 +1,27 @@
 import PageControls from './modules/PageControls';
 import $ from 'jquery';
+const pageControls = new PageControls();
 
-let pageControls = new PageControls();
 
-var loadResources = function(){
-  $.getJSON("https://api.whatdoestrumpthink.com/api/v1/quotes/random", function(json) {
-    let response = JSON.stringify(json.message);
-    let replaceRegex = response.replace(/[\s]+/g, '%20');
-    let quoteToLink = "https://twitter.com/share?url=https%3A%2F%2Ftrumpspiration.com&text=Remember these \"inspiring\" words from Trump? " + replaceRegex;
-    pageControls.quoteText.html(response);
-    pageControls.tweetButton.attr("href", quoteToLink);
-  });
-  $.getJSON("https://api.unsplash.com/photos/random/?client_id=a476048633d1fb233b59080593173ffa6de15ab3670e2b9ddbe593d3137d2e31", function(json) {
-    pageControls.quoteArea.css("background-image", "url(" + JSON.stringify(json.urls.regular) + ")" );
-    // pageControls.aboutArea.css("background-image", "url(" + JSON.stringify(json.urls.regular) + ")" );
-  });
+let loadResources = async () => {
+
+  let quoteJSON = await fetch("https://api.whatdoestrumpthink.com/api/v1/quotes/random");
+  let quote = await quoteJSON.json();
+
+  let imageJSON = await fetch("https://api.unsplash.com/photos/random/?client_id=a476048633d1fb233b59080593173ffa6de15ab3670e2b9ddbe593d3137d2e31");
+  let image = await imageJSON.json();
+
+  let newTwitterURI = encodeURIComponent(`Remember when Trump said this? "${quote.message}" #impeachnow #notmypresident`);
+  let quoteToLink = "https://twitter.com/share?url=https%3A%2F%2Ftrumpspiration.com&text=" + newTwitterURI;
+
+  pageControls.tweetButton.attr("href", quoteToLink);
+
+  console.log(image.urls.regular);
+
+  pageControls.quoteText.html(quote.message);
+  pageControls.quoteArea.css("background-image", `url(${image.urls.regular})`);
 }
 
 $(document).ready(loadResources);
+
 pageControls.quoteButton.on("click", loadResources);
